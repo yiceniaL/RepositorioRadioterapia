a8ecb0956995caca284ddc0a370cd7df941d81bb
yiceniaL <75313316+yiceniaL@users.noreply.github.com>
2021-05-31 18:44:23 -0400

    saver

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -48,7 +48,7 @@ body {font-size:16px;}
   <div class="w3-container w3-margin-left" id="inicio">
     <h1 class="w3-xxxlarge" style="margin-top:70px"><b>Repositorio de Radioterapia</b></h1>
   </div>
-
+<!-- Botones -->
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
 
@@ -60,8 +60,77 @@ body {font-size:16px;}
   </p>
 </div>
 
+<!-- Estadisticos -->
+<style>
+* {
+  box-sizing: border-box;
+}
+/* Float four columns side by side */
+.column {
+  float: left;
+  width: 15%;
+  padding: 0 10px;
+}
+/* Remove extra left and right margins, due to padding */
+.row {margin: 10 -5px;}
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+/* Responsive columns */
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+    display: block;
+    margin-bottom: 20px;
+  }
+}
+/* Style the counter cards */
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  padding: 15px;
+  text-align: center;
+  background-color: #0E6655;
+  color: white;
+}
+</style>
+<div style="margin-top:40px" class="row">
+  <div style="margin-left:150px" class="column">
+    <div class="card">
+      <h1>X</h1>
+      <p style="font-size:20px;">Casos Publicados</p>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+      <h1>X</h1>
+      <p style="font-size:20px;">Propuestas Publicadas</p>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+    <h1>X</h1>
+    <p style="font-size:20px;">Instituciones con Casos</p>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+      <h1>X</h1>
+      <p style="font-size:20px;">Autores con Propuestas</p>
+    </div>
+  </div>
+  <div class="column">
+      <div class="card">
+        <h1>X</h1>
+        <p style="font-size:20px;">Total de Descargas</p>
+      </div>
+    </div>
+</div>
+
  <!-- Detalle de los primeros textos -->
-<div class="w3-container w3-margin-left" style="margin-top:100px">
+<div class="w3-container w3-margin-left" style="margin-top:20px">
     <hr style="width:80px;border:5px  solid white" id="comoPublicar" class="w3-round">
     <h1 class="w3-xxlarge"  style="margin-top:50px"><b>¿Cómo publicar?</b></h1>
     <hr style="width:80px;border:5px  solid teal" class="w3-round">
@@ -101,7 +170,7 @@ body {font-size:16px;}
     <h1 class="w3-xxlarge" style="margin-top:50px"><b>Quienes Somos</b></h1>
     <hr style="width:80px;border:5px  solid teal" class="w3-round">
     <h3 style="margin-top:40px">Este repositorio web tiene el principal objetivo de implementar algoritmos inteligentes aplicados a la toma de decisiones con múltiples objetivos, variables y restricciones, esto será aplicado a tratamientos para combatir el cáncer, y permitirá a los médicos tomar la mejor opción de tratamiento para cada paciente.</h3>
-    <h3 style="margin-top:20px">Este proyecto está liderado por el PhD. Guillermo Cabrera, quien se adjudicó un proyecto FONDECYT, Fondo Nacional de Desarrollo Científico y Tecnológico, es parte del Gobierno de Chile y depende de la Comisión Nacional de Investigación Científica y Tecnológica. En colaboración con alumnos de la Universidad Católica del Norte, se llevó a cabo este repositorio con la finalidad de subir, visualizar y/o descargar informes de casos clínicos tratados.</h3>
+    <h3 style="margin-top:20px">Este proyecto está liderado por el PhD. Guillermo Cabrera, quien se adjudicó un proyecto FONDECYT, Fondo Nacional de Desarrollo Científico y Tecnológico, es parte del Gobierno de Chile es un fondo del gobierno de Chile y depende de la Agencia nacional de investigacion y desarrollo. En colaboración con alumnos de la Universidad Católica del Norte, se llevó a cabo este repositorio con la finalidad de subir, visualizar y/o descargar informes de casos clínicos tratados.</h3>
     <h3 style="margin-top:20px">La finalidad de la plataforma es incentivar la interacción entre investigadores de la radioterapia y/o médicos dedicados al tratamiento del cáncer, así pudiendo compartir sus conocimientos y experiencia al momento de decidir la mejor manera de combatir la enfermedad.</h3>
 </div>
  <!-- Detalle de los terceros textos -->
