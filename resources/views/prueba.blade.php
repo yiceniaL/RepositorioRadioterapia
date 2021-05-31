20619e55b72e5e5b651019c5002f97de91cc7c7f
yiceniaL <75313316+yiceniaL@users.noreply.github.com>
2021-05-30 21:44:14 -0400

    saver

---
+++ resources/views/prueba.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body,h1,h2,h3,h4,h5 {font-family: "arial", sans-serif}
+body {font-size:16px;}
+.w3-half img:hover{opacity:1}
+</style>
+</head>
+<body>
+
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+
+<div style="margin-top:420px" class="w3-container">
+
+  <p>
+    <button a href="http://127.0.0.1:8000/tratamientos" style="margin-left:220px" class="w3-xxlarge w3-bar-item w3-button w3-hover-none w3-light-grey w3-hover-text-teal">Tratamientos</a></button>
+    <button a href="http://127.0.0.1:8000/casos" class="w3-xxlarge w3-bar-item w3-button w3-hover-none w3-light-grey w3-hover-text-teal">Casos</a></button>
+    <button a href="http://127.0.0.1:8000/formularioCaso"class="w3-xxlarge w3-bar-item w3-button w3-hover-none w3-light-grey w3-hover-text-teal">Subir un Caso</a></a></button>
+  </p>
+
+</div>
+
+</body>
+</html>
+
