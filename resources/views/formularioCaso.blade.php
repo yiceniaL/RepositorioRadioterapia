47e7143f4afddf4fdb55bb79a2509d62c238baa9
yiceniaL <75313316+yiceniaL@users.noreply.github.com>
2021-06-07 18:48:00 -0400

    saver

--- resources/views/formularioCaso.blade.php
+++ resources/views/formularioCaso.blade.php
@@ -755,12 +755,26 @@ x</button></h3>
     <div class="inputs"></div>
     <div class="w3-row w3-center">
     <div class="w3-container w3-quarter"></div>
-    <div class="w3-container w3-quarter w3-margin-bottom w3-tooltip">
-    <button type="button" onclick="nuevo();" class="w3-button w3-large w3-circle w3-grey">
-        <span style="position:absolute;left:-50px;top:10px" class="w3-text w3-tag w3-large w3-white w3-tiny">
-        Agregar otro indicador
-        </span>+</button>
+    <p id="demo"></p>
     </div>
+    <script>
+    var i;
+    var n = 5
+    var t1 ='<div class="w3-row w3-center w3-container w3-quarter w3-col s1">'
+    ;
+    var t2 ='<div class="w3-row-padding w3-margin-left">'+
+    '    <div class="w3-col s3"><h3>1 Nombre del Indicador*</h3></div>'+
+    '    <div class="w3-col s3"><select class="w3-select w3-border " name="option">'+
+    '    <option value="" disabled selected>Selecciona el indicador</option><option value="1">lista5</option><option value="2">Option 2</option></select></div></div>'
+    ;
+    var t3 = '<div class="w3-row-padding w3-margin-left">'+
+    '    <div class="w3-col s3"><h3>1 Valor del indicador*</h3></div>'+
+    '    <div class="w3-col s3"><input class="w3-input w3-border" type="text" placeholder="Ingresa el valor del indicador"></div></div>'
+    ;
+    for (i = 0; i < n+1; i++)  {
+        document.getElementById("demo").innerHTML+=i+t1+t2+t3;
+    </script>
+
     </section1>` 
     );   
 }
--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -1,14 +1,35 @@
-<!DOCTYPE html>  
-    <html>  
-    <head>  
-        <title>Bucle For de JavaScript en Estrada Web Group</title>  
-    </head>  
-    <body>
-        <h1>¿Cuantos años tienes?</h1>
-        <select>
-            <?php foreach (range(1, 10) as $num): ?>
-            <a>1</a>
-            <?php endforeach; ?>
-        </select>
-    </body>  
-    </html>  
+
+<html>
+  <head>
+    <title>Cómo ejecutar una función desde un enlace o link HTML con Java Script</title>
+  </head>
+
+<button onclick="fill_1 (this);">boton 2 </button> 
+
+<script>
+
+function fill_1(x){
+    var i;
+    var n = 5
+    var t1 ='<div class="w3-row w3-center w3-container w3-quarter w3-col s1">'
+    ;
+    var t2 ='<div class="w3-row-padding w3-margin-left">'+
+    '    <div class="w3-col s3"><h3>1 Nombre del Indicador*</h3></div>'+
+    '    <div class="w3-col s3"><select class="w3-select w3-border " name="option">'+
+    '    <option value="" disabled selected>Selecciona el indicador</option><option value="1">lista5</option><option value="2">Option 2</option></select></div></div>'
+    ;
+    var t3 = '<div class="w3-row-padding w3-margin-left">'+
+    '    <div class="w3-col s3"><h3>1 Valor del indicador*</h3></div>'+
+    '    <div class="w3-col s3"><input class="w3-input w3-border" type="text" placeholder="Ingresa el valor del indicador"></div></div>'
+    ;
+    for (i = 0; i < n+1; i++)  {
+        document.getElementById("demo").innerHTML+=i+t1+t2+t3;
+    }
+}
+
+</script>
+<p id="demo"></p>
+
+</body>
+
+</html>
