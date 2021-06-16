fc1fc769bd501227f0e7f0a6b7b5b8d919b7c2dc
yiceniaL <75313316+yiceniaL@users.noreply.github.com>
2021-06-15 22:35:41 -0400

    saver

--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -12,11 +12,23 @@ body {font-size:16px;}
 .w3-half img{margin-bottom:-6px;margin-top:16px;opacity:0.8;cursor:pointer}
 .w3-half img:hover{opacity:1}
 </style>
-<html>
 <body>
 
+<a href="#comoPublicar" >¿Cómo publicar?</a>
 
+<div class="w3-padding-64"></div>
+<div class="w3-padding-64"></div>
+<div class="w3-padding-64"></div>
+<div class="w3-padding-64"></div>
+<div class="w3-padding-64"></div>
+<div class="w3-padding-64"></div>
+<div class="w3-padding-64"></div>
+<div class="w3-padding-64"></div>
 
-
-</body>
-</html>
+ <!-- Detalle de los primeros textos -->
+<div class="w3-container w3-margin-left" style="margin-top:20px">
+    <hr style="width:80px;border:5px  solid white" id="comoPublicar" class="w3-round">
+    <h1 class="w3-xxlarge"  style="margin-top:50px"><b>¿Cómo publicar?</b></h1>
+    <hr style="width:80px;border:5px  solid teal" class="w3-round">
+    <h3 style="margin-top:40px">Para subir información a la plataforma, primero debes identificar qué tipo de información quieres subir:</h3>
+ </div>
